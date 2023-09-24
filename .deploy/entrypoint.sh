293268ed806cb603c9497f96f03c11f3c56ef4af
Jaw <58354193+wajeht@users.noreply.github.com>
2023-09-24 03:12:12 -0500

    feat: productin script

--- .deploy/entrypoint.sh
+++ .deploy/entrypoint.sh
@@ -7,7 +7,11 @@ composer dump-autoload --no-interaction --no-dev --optimize
 echo "🎬 artisan commands"
 
 # 💡 Group into a custom command e.g. php artisan app:on-deploy
+npm run build
 php artisan migrate --no-interaction --force
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
 
 echo "🎬 start supervisord"
 
